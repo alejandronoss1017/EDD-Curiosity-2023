c46bad6c04f170009550b0034a4329b5b1f8f0b3
Camilo <alejandro_noss@hotmail.com>
2023-02-21 23:56:16 -0500

    Modificacion de atributos, eliminacion de filesystem, y nuevos metodos

--- src/curiosity/Curiosity.hpp
+++ src/curiosity/Curiosity.hpp
@@ -2,24 +2,40 @@
 #define CURIOSITY_HPP
 
 #include <string>
-#include <queue>
+#include <list>
 #include <fstream>
-#include <filesystem>
 #include <iostream>
+
+#include "../commonFunctions/commonFunctions.hpp"
+#include "../element/Element.hpp"
 
 using namespace std;
 
 class Curiosity
 {
 private:
-    queue<string> commands;
+    list<string> commands;
+    list<Element> elements;
+    // TODO: Crear lista de analisis
 
 public:
     Curiosity(/* args */);
     ~Curiosity();
 
-    queue<string> GetCommands() const;
-    void SetCommands(queue<string> commands);
+    /* //TODO: Cambiar la lectura del archivo a la shell y hacer validacion
+        de cada comando en el archivo
+    */
+    void uploadCommands(string filename);
+    void addCommand(string command);
+
+    void uploadElements(string filename);
+    void addElement(Element element);
+
+    /*  Getters and Setters */
+    list<string> GetCommands() const;
+    void SetCommands(list<string> commands);
+    list<Element> GetElements() const;
+    void SetElements(list<Element> elements);
 };
 
 #include "Curiosity.cxx"
