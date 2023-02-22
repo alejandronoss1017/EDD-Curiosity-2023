6995fe7c62debd18bc110b523bc0f85c76b8fb1c
Camilo Nossa <alejandro_noss@hotmail.com>
2023-02-22 15:44:09 -0500

    Nueva clase de comando

---
+++ src/command/Command.cxx
@@ -0,0 +1,9 @@
+#include "Command.hpp"
+
+Command::Command()
+{
+}
+
+Command::~Command()
+{
+}
---
+++ src/command/Command.hpp
@@ -0,0 +1,20 @@
+#if !defined(COMMAND_HPP)
+#define COMMAND_HPP
+
+#include <string>
+
+using namespace std;
+
+class Command
+{
+private:
+    // TODO: Implementar clase de comandos en vez de utilizar strings
+    /* data */
+public:
+    Command(/* args */);
+    ~Command();
+};
+
+#include "Command.cxx"
+
+#endif // COMMAND_HPP
