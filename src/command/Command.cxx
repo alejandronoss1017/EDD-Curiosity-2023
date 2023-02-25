009cb187aaf23ce5086696a5bfc0fe0b5032e048
Camilo <alejandro_noss@hotmail.com>
2023-02-25 18:01:10 -0500

    Implementacion de la nueva clase Command

--- src/command/Command.cxx
+++ src/command/Command.cxx
@@ -1,9 +1,53 @@
 #include "Command.hpp"
 
-Command::Command()
+Command::Command() {}
+
+Command::Command(commandTypes command, vector<string> params)
 {
+    this->command = command;
+    this->params = params;
 }
 
 Command::~Command()
 {
+}
+
+commandTypes Command::GetCommand() const
+{
+    return command;
+}
+
+void Command::SetCommand(commandTypes command)
+{
+    command = command;
+}
+
+vector<string> Command::GetParams() const
+{
+    return params;
+}
+
+void Command::SetParams(vector<string> params)
+{
+    params = params;
+}
+
+ostream &operator<<(ostream &os, const vector<string> &theV)
+{
+    for (auto i = theV.begin(); i != theV.end(); i++)
+    {
+        if (*i == theV.back())
+        {
+            os << *i;
+            break;
+        }
+        os << *i << " ";
+    }
+
+    return os;
+}
+ostream &operator<<(ostream &os, const Command &command)
+{
+    os << commandTypes2Str(command.command) << " " << command.params;
+    return os;
 }
--- src/command/Command.hpp
+++ src/command/Command.hpp
@@ -2,6 +2,9 @@
 #define COMMAND_HPP
 
 #include <string>
+#include <vector>
+
+#include "../enums/commandTypes.hpp"
 
 using namespace std;
 
@@ -9,10 +12,27 @@ class Command
 {
 private:
     // TODO: Implementar clase de comandos en vez de utilizar strings
-    /* data */
+    commandTypes command;
+    vector<string> params;
+
 public:
-    Command(/* args */);
+    Command();
+
+    Command(commandTypes command, vector<string> params);
+
     ~Command();
+
+    commandTypes GetCommand() const;
+
+    void SetCommand(commandTypes command);
+
+    vector<string> GetParams() const;
+
+    void SetParams(vector<string> params);
+
+    /*  Operator overloaods  */
+    friend ostream &operator<<(ostream &os, const Command &command);
+    friend ostream &operator<<(ostream &os, const vector<string> &theV);
 };
 
 #include "Command.cxx"
